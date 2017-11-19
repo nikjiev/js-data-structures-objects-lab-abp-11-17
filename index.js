bd73fd33c6553b70e046d8ce9e977b4331a7ef83
Nik Jiev <n.arabadjiev@gmail.com>
2017-11-19 19:07:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const driver = {
 };
 
 function updateDriverWithKeyAndValue(driver, key, value) {
-  return Object.assign([], driver, {[key]:value});
+  return Object.assign({}, driver, {[key]:value});
 }
