cfc30eaaaae4ebd2a352afdc8029e9ed382db0fa
Nik Jiev <n.arabadjiev@gmail.com>
2017-11-19 19:31:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ function updateDriverWithKeyAndValue(driver, key, value) {
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   return Object.assign(driver, {name: 'Sam', address: '12 Broadway'});
 }
+
+function deleteFromDriverByKey(driver, key) {
+  const newDriver = Object.assing({}, driver);
+  return delete newDriver[key]
+}
